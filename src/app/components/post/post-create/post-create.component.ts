a56579be44d26d8a0225333ff48d8bee3a89126a
duynd1895 <duynv1895@gmail.com>
2020-04-20 17:11:39 +0700

    Sua dinh dang anh

--- src/app/components/post/post-create/post-create.component.ts
+++ src/app/components/post/post-create/post-create.component.ts
@@ -28,9 +28,7 @@ export class PostCreateComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      title: new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3)]
-      }),
+      title: new FormControl(null, { validators: [Validators.required, Validators.minLength(3)]}),
       content: new FormControl(null, { validators: [Validators.required] }),
       image: new FormControl(null, {
         validators: [Validators.required],
